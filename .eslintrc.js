18acac9910db7303a675e89090fed93390663734
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-10-07 21:57:25 +0900

    eslintルール見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
+  plugins: ["unused-imports"],
   extends: [
     "@cybozu/eslint-config/presets/react-typescript-prettier",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/jsx-runtime",
   ],
+  rules: {
+    "import/order": ["error", { "newlines-between": "always" }],
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
 };
